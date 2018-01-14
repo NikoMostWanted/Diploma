091db9a9a1768e6f3aae9b3fb97e3d4223db72a0
Niko Fedorik <kolu4ka02031995@gmail.com>
2018-01-14 19:46:45 +0200

    delete condition

--- models/Sections.php
+++ models/Sections.php
@@ -59,33 +59,22 @@ class Sections extends \yii\db\ActiveRecord
         return $cats;
     }
 
-    public static function build_tree($cats, $sid, $only_parent = false)
+    public static function build_tree($cats, $sid)
     {
         if(is_array($cats) && isset($cats[$sid]))
         {
             $tree = '<ul>';
-            if($only_parent == false)
+
+            foreach($cats[$sid] as $cat)
             {
-                foreach($cats[$sid] as $cat)
-                {
-                    $tree .= '<li>'.$cat['name'].' '.$cat['alias'];
-                    $tree .= Html::a('<span class="glyphicon glyphicon-pencil"></span>', ['admin/section-edit', 'id' => $cat['id']], ['class' => 'btn btn-success']);
-                    $tree .= Html::a('<span class="glyphicon glyphicon-remove"></span>', ['admin/section-delete', 'id' => $cat['id']], ['class' => 'btn btn-danger']);
-                    $tree .= Html::a('<span class="glyphicon glyphicon-plus"></span>', ['admin/section-create', 'id' => $cat['id']], ['class' => 'btn btn-success']);
-                    $tree .= self::build_tree($cats,$cat['id']);
-                    $tree .= '</li>';
-                }
-            }
-            elseif(is_numeric($only_parent))
-            {
-                $cat = $cats[$sid][$only_parent];
-                $tree .= '<li>'.$cat['name'].' #'.$cat['alias'];
+                $tree .= '<li>'.$cat['name'].' '.$cat['alias'];
                 $tree .= Html::a('<span class="glyphicon glyphicon-pencil"></span>', ['admin/section-edit', 'id' => $cat['id']], ['class' => 'btn btn-success']);
                 $tree .= Html::a('<span class="glyphicon glyphicon-remove"></span>', ['admin/section-delete', 'id' => $cat['id']], ['class' => 'btn btn-danger']);
                 $tree .= Html::a('<span class="glyphicon glyphicon-plus"></span>', ['admin/section-create', 'id' => $cat['id']], ['class' => 'btn btn-success']);
-                $tree .=  self::build_tree($cats,$cat['id']);
+                $tree .= self::build_tree($cats,$cat['id']);
                 $tree .= '</li>';
             }
+
             $tree .= '</ul>';
         }
         else return null;
